29338c3a50de87d2626d750af71011614f6a902e
Jeonghaeng Heo <53353435+Jeong93@users.noreply.github.com>
2020-11-04 23:19:22 +0900

    Add files via upload

---
+++ mock-data.js
@@ -0,0 +1,176 @@
+let mock = [{
+    id: 1,
+    name: "Rodger Garden",
+    gender: "m",
+    exerciseFor: "back",
+    membership: "basic",
+  },
+  {
+    id: 2,
+    name: "Carlita Beal",
+    gender: "f",
+    exerciseFor: "abs",
+    membership: "fit"
+  },
+  {
+    id: 3,
+    name: "Riley Bowdoin",
+    gender: "m",
+    exerciseFor: "back",
+    membership: "fit"
+  },
+  {
+    id: 4,
+    name: "Neida Voris",
+    gender: "f",
+    exerciseFor: "legs",
+    membership: "pro"
+  },
+  {
+    id: 5,
+    name: "Ezekiel Beal",
+    gender: "m",
+    exerciseFor: "butt",
+    membership: "pro"
+  },
+  {
+    id: 6,
+    name: "Carin Mullan",
+    gender: "f",
+    exerciseFor: "legs",
+    membership: "basic"
+  },
+  {
+    id: 7,
+    name: "Abraham Voris",
+    gender: "m",
+    exerciseFor: "chest",
+    membership: "fit"
+  },
+  {
+    id: 8,
+    name: "Jolanda Simington",
+    gender: "f",
+    exerciseFor: "arms",
+    membership: "fit"
+  },
+  {
+    id: 9,
+    name: "Burton Sydnor",
+    gender: "m",
+    exerciseFor: "shoulders",
+    membership: "pro"
+  },
+  {
+    id: 10,
+    name: "Carin Beal",
+    gender: "f",
+    exerciseFor: "arms",
+    membership: "pro"
+  },
+  {
+    id: 11,
+    name: "Bruce Beal",
+    gender: "m",
+    exerciseFor: "shoulders",
+    membership: "fit"
+  },
+  {
+    id: 12,
+    name: "Cheryle Cubbage",
+    gender: "f",
+    exerciseFor: "legs",
+    membership: "basic"
+  },
+  {
+    id: 13,
+    name: "Haywood Wilhite",
+    gender: "m",
+    exerciseFor: "arms",
+    membership: "basic"
+  },
+  {
+    id: 14,
+    name: "Jolanda Northington",
+    gender: "f",
+    exerciseFor: "chest",
+    membership: "basic"
+  },
+  {
+    id: 15,
+    name: "Paris Demarco",
+    gender: "m",
+    exerciseFor: "butt",
+    membership: "basic"
+  },
+  {
+    id: 16,
+    name: "Alpha Anker",
+    gender: "f",
+    exerciseFor: "arms",
+    membership: "basic"
+  },
+  {
+    id: 17,
+    name: "Huey Simington",
+    gender: "m",
+    exerciseFor: "shoulders",
+    membership: "pro"
+  },
+  {
+    id: 18,
+    name: "Mallie Carmouche",
+    gender: "f",
+    exerciseFor: "arms",
+    membership: "fit"
+  },
+  {
+    id: 19,
+    name: "Tyree Voris",
+    gender: "m",
+    exerciseFor: "abs",
+    membership: "basic"
+  },
+  {
+    id: 20,
+    name: "Freda Amsler",
+    gender: "f",
+    exerciseFor: "arms",
+    membership: "basic"
+  },
+  {
+    id: 21,
+    name: "Ezra Beal",
+    gender: "m",
+    exerciseFor: "legs",
+    membership: "fit"
+  },
+  {
+    id: 22,
+    name: "Shae Gaudet",
+    gender: "f",
+    exerciseFor: "back",
+    membership: "pro"
+  },
+  {
+    id: 23,
+    name: "Freddie Heckel",
+    gender: "m",
+    exerciseFor: "back",
+    membership: "basic"
+  },
+  {
+    id: 24,
+    name: "Cicely Sorensen",
+    gender: "f",
+    exerciseFor: "abs",
+    membership: "pro"
+  },
+  {
+    id: 25,
+    name: "Ahmad Hunt",
+    gender: "m",
+    exerciseFor: "back",
+    membership: "basic"
+  }
+]
